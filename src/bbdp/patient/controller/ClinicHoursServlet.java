10f01912963c80b858abe8665fe853899308cff8
ChuHueiYun <puddingdog1996@gmail.com>
2018-07-18 10:07:18 +0800

    718

---
+++ src/bbdp/patient/controller/ClinicHoursServlet.java
@@ -0,0 +1,28 @@
+package bbdp.patient.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+
+import bbdp.patient.model.ClinicHoursServer;
+
+public class ClinicHoursServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		String option = request.getParameter("option");
+		DataSource datasource = (DataSource) getServletContext().getAttribute("db");
+		
+		if (option.equals("getHospital")) {		//取得現有醫生的醫院科別資料
+			response.getWriter().print(ClinicHoursServer.getHospital(datasource));
+		} else if (option.equals("getClinicHours")) {		//取得門診時間
+			String doctorID = request.getParameter("doctorID");
+			response.getWriter().print(ClinicHoursServer.getClinicHours(datasource, doctorID));
+		}
+	}
+}
---
+++ src/bbdp/patient/controller/NotificationSettingServlet.java
@@ -0,0 +1,29 @@
+package bbdp.patient.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+
+import bbdp.patient.model.NotificationSettingServer;
+
+public class NotificationSettingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		String option = request.getParameter("option");
+		DataSource datasource = (DataSource) getServletContext().getAttribute("db");
+		String patientID = request.getParameter("patientID");
+		
+		if (option.equals("getNotificationSetting")) {		//取得通知設定
+			response.getWriter().print(NotificationSettingServer.getNotificationSetting(datasource, patientID));
+		} else if (option.equals("modifyNotificationSetting")) {		//修改通知設定
+			String notification = request.getParameter("notification");
+			NotificationSettingServer.modifyNotificationSetting(datasource, patientID, notification);
+		}
+	}
+}
